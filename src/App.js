6198bc03d835378032332dd2532df410553fdd52
João Victor Garcia <125100615+JoaoVictorGarcia2@users.noreply.github.com>
2024-11-21 11:53:26 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './pages/Login';
-import SignUp from './pages/SingnUp';
+import Login from './Login';
+import SignUp from './SingnUp';
 
 const App = () => {
   return (
