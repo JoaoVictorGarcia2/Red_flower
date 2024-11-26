5d2835d067078de484be0c469974e1b230839a5c
JoãoVictor Garcia2 <joaovictorgarcia514@gmail.com>
2024-11-26 11:55:09 -0300

    imagens dos cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -53,32 +53,32 @@ export default function Home() {
       {
         name: "Memória DDR4 XPG Gammix D10, 16GB (2x8GB) 3200Mhz, Black",
         price: "R$ 288,90à vista",
-        image: "https://via.placeholder.com/150",
+        image: "https://cdn.shopify.com/s/files/1/0097/7633/2855/products/71hWYp4YB-L._SL1500.jpg?v=1625089646&width=1445",
       },
       {
-        name: "Controle Microsoft Xbox Robot White, Wireless, Branco",
+        name: "Controle Microsoft Xbox Robot White, Wireless, Preto",
         price: "R$ 389,90à vista",
-        image: "https://via.placeholder.com/150",
+        image: "https://http2.mlstatic.com/controle-elite-xbox-one-microsoft-original-lacrado-verso-2-D_NQ_NP_644264-MLB40593829134_012020-F.jpg",
       },
       {
         name: "Cooler Para Processador DeepCool High Performance AK400",
         price: "R$ 149,90à vista",
-        image: "https://via.placeholder.com/150",
+        image: "https://img.terabyteshop.com.br/produto/g/cooler-para-processador-deepcool-ak400-120mm-intel-amd-r-ak400-bknnmn-g-1_144160.jpg",
       },
       {
         name: "Microfone Gamer Fifine Ampligame A6T, RGB, USB",
         price: "R$ R$ 279,90à vista",
-        image: "https://via.placeholder.com/150",
+        image: "https://img.terabyteshop.com.br/produto/g/microfone-gamer-fifine-ampligame-a6t-rgb-usb-black-com-braco-articulado_169456.jpg",
       },
       {
         name: "Notebook Lenovo Thinkpad Yoga G2 Intel Core i5",
         price: "R$ 3.899,99à vista",
-        image: "https://via.placeholder.com/150",
+        image: "https://th.bing.com/th/id/R.55920359762726c3f93b9bfdca13ad98?rik=QmFQOIwi2%2f6toA&pid=ImgRaw&r=0",
       },
       {
         name: "Monitor Gamer Duex, 27 Pol, Full HD, IPS, 240Hz",
         price: "R$ 819,90à vista",
-        image: "https://via.placeholder.com/150",
+        image: "https://media.pichau.com.br/media/catalog/product/cache/2f958555330323e505eba7ce930bdf27/d/x/dx270ghq-240hz1.jpg",
       },
     ].map((product, index) => (
       <div key={index} className="card">
